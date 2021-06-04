135be92ac080380fa33eb2d8622706d16905d8ab
demogorii <bathoryaria@gmail.com>
2021-06-03 22:48:16 -0400

    Initial Commit

---
+++ module/actor/actor-sheet.js
@@ -0,0 +1,284 @@
+/**
+ * Extend the basic ActorSheet with some very simple modifications
+ * @extends {ActorSheet}
+ */
+export class PeculiarTalesActorSheet extends ActorSheet {
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["peculiartales", "sheet", "actor"],
+      template: "systems/peculiartales/templates/actor/actor-sheet.html",
+      width: 650,
+      height: 700,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    data.dtypes = ["String", "Number", "Boolean"];
+    for (let attr of Object.values(data.data.attributes)) {
+      attr.isCheckbox = attr.dtype === "Boolean";
+    }
+
+    // Prepare items.
+    if (this.actor.data.type == 'character') {
+      this._prepareCharacterItems(data);
+    }
+
+    return data;
+  }
+
+  /**
+   * Organize and classify Items for Character sheets.
+   *
+   * @param {Object} actorData The actor to prepare.
+   *
+   * @return {undefined}
+   */
+  _prepareCharacterItems(sheetData) {
+    const actorData = sheetData.actor;
+
+    // Initialize containers.
+    const gear = [];
+    const connections = [];
+    const features = [];
+
+    // Iterate through items, allocating to containers
+    // let totalWeight = 0;
+    for (let i of sheetData.items) {
+      let item = i.data;
+      i.img = i.img || DEFAULT_TOKEN;
+      // Append to gear.
+      if (i.type === 'item') {
+        gear.push(i);
+      }
+      // Append to features.
+      else if (i.type === 'connection') {
+        connections.push(i);
+      }
+      // Append to spells.
+      else if (i.type === 'feature') {
+        features.push(i);
+      }
+    }
+
+    // Assign and return
+    actorData.gear = gear;
+    actorData.features = features;
+    actorData.connections = connections;
+    actorData.game = game;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Add Inventory Item
+    html.find('.item-create').click(this._onItemCreate.bind(this));
+
+    // Update Inventory Item
+    html.find('.item-edit').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find('.item-delete').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      this.actor.deleteOwnedItem(li.data("itemId"));
+      li.slideUp(200, () => this.render(false));
+    });
+
+    // Rollable abilities.
+    html.find('.rollable').click(this._onRoll.bind(this));
+
+    // Drag events for macros.
+    if (this.actor.owner) {
+      let handler = ev => this._onDragStart(ev);
+      html.find('li.item').each((i, li) => {
+        if (li.classList.contains("inventory-header")) return;
+        li.setAttribute("draggable", true);
+        li.addEventListener("dragstart", handler, false);
+      });
+    }
+  }
+
+  /**
+   * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onItemCreate(event) {
+    event.preventDefault();
+    const header = event.currentTarget;
+    // Get the type of item to create.
+    const type = header.dataset.type;
+    // Grab any data associated with this control.
+    const data = duplicate(header.dataset);
+    // Initialize a default name.
+    const name = `New ${type.capitalize()}`;
+    // Prepare the item object.
+    const itemData = {
+      name: name,
+      type: type,
+      data: data
+    };
+    // Remove the type from the dataset since it's in the itemData.type prop.
+    delete itemData.data["type"];
+
+    // Finally, create the item!
+    return this.actor.createOwnedItem(itemData);
+  }
+
+  /**
+   * Handle clickable rolls.
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onRoll(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+
+    if (dataset.roll) {
+
+      async function drawCardWait(){
+        let cardid = game.playerdeck.infinteDraw();
+        let card = await game.playerdeck.getCardData(cardid);
+        return card;
+      }
+
+      drawCardWait().then(boostedCard => {
+        drawCardWait().then(card => {
+          let result = this._getCardDetails(card, dataset.label);
+
+          let value = result.cardvalue;
+          let pretext = dataset.label[0].toUpperCase() + dataset.label.slice(1).toLowerCase();
+
+          let label = dataset.label ? `<b>${pretext} skill check...</b>` : '';
+
+          let labelPreboost = label + '<br/>' + "Drew <b>" + result.cardname + "</b>!"
+
+          let labelPostboost = "" ;
+
+          if (result.boosting){
+            let boostingCardDetails = this._getCardDetails(boostedCard, dataset.label);
+            labelPostboost = '<br /><span style="color:red"><b>Boosted</b> </span> with ' + boostingCardDetails.cardname + "!";
+            value = value + boostingCardDetails.cardvalue;
+          }
+
+          let dataroll = {...this.actor.data.data, drawvalue: value}
+          let roll = new Roll(dataset.roll, dataroll);
+
+          let flavor = labelPreboost + labelPostboost;
+
+          roll.roll().toMessage({
+            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            flavor: flavor
+          });
+        });
+      });
+    }
+  }
+
+  _getCardDetails(card, label){
+    let cardname = "";
+    let cardvalue = 0;
+    let boosting = false;
+    let skillsuit = "";
+    if (this.actor.data.data.abilities[label]){
+        skillsuit = this.actor.data.data.abilities[label].suit;
+    }
+
+    if (card.value === "JOKER") {
+      cardname = "a JOKER";
+      cardvalue = 10;
+    }
+    else if (card.value === 11){ // TODO : FATIGUE CONSEQUENCES...
+      cardname = "a Jack of " + card.suit;
+      cardvalue = 5;
+      if (card.suit === skillsuit){
+        boosting = true;
+      }
+      let status = this._updateCardForStatus(card, label, cardname, cardvalue);
+      cardname = status.cardname;
+      cardvalue = status.cardvalue;
+    }
+    else if (card.value === 12){
+      cardname = "a Queen of " + card.suit;
+      cardvalue = 5;
+      if (card.suit === skillsuit){
+        boosting = true;
+      }
+      let status = this._updateCardForStatus(card, label, cardname, cardvalue);
+      cardname = status.cardname;
+      cardvalue = status.cardvalue;
+    }
+    else if (card.value === 13){
+      cardname = "a King of " + card.suit;
+      cardvalue = 5;
+      if (card.suit === skillsuit){
+        boosting = true;
+      }
+      let status = this._updateCardForStatus(card, label, cardname, cardvalue);
+      cardname = status.cardname;
+      cardvalue = status.cardvalue;
+    }
+    else if (card.value === 1){
+      cardname = "an Ace of " + card.suit;
+      cardvalue = 1;
+      if (card.suit === skillsuit){
+        boosting = true;
+      }
+    }
+    else{
+      cardname = "a " + card.value + " of " + card.suit;
+      cardvalue = card.value;
+      if (card.suit === skillsuit){
+        boosting = true;
+      }
+    }
+
+    return { cardname: cardname, cardvalue: cardvalue, boosting: boosting};
+  }
+
+  _updateCardForStatus(card, label, cardname, cardvalue){
+    if (this.actor.data.data.attributes.injured.value)
+    {
+      if (label === "dexterity" || label === "fitness")
+      {
+        cardvalue = 0;
+        cardname = "<s>" + cardname + "</s> (injured)";
+      }
+    }
+    if (this.actor.data.data.attributes.fatigued.value)
+    {
+      if (label === "smarts" || label === "charisma")
+      {
+        cardvalue = 0;
+        cardname = "<s>" + cardname + "</s> (fatigued)";
+      }
+    }
+    if (this.actor.data.data.attributes.taxed.value)
+    {
+      if (label === "assets")
+      {
+        cardvalue = 0;
+        cardname = "<s>" + cardname + "</s> (taxed)";
+      }
+    }
+
+    return {cardname: cardname, cardvalue: cardvalue};
+  }
+}
---
+++ module/peculiartales.js
@@ -0,0 +1,152 @@
+// Import Modules
+import { PeculiarTalesActor } from "./actor/actor.js";
+import { PeculiarTalesActorSheet } from "./actor/actor-sheet.js";
+import { PeculiarTalesItem } from "./item/item.js";
+import { PeculiarTalesItemSheet } from "./item/item-sheet.js";
+import { Deck } from "./../../../modules/cardsupport/scripts/deck.js"
+
+Hooks.once('init', async function() {
+
+  game.peculiartales = {
+    PeculiarTalesActor,
+    PeculiarTalesItem,
+    rollItemMacro
+  };
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "@initiativeRoll",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = PeculiarTalesActor;
+  CONFIG.Item.entityClass = PeculiarTalesItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("peculiartales", PeculiarTalesActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("peculiartales", PeculiarTalesItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
+  });
+});
+
+Hooks.once("decks.ready", async function(){
+ const gmdeckid = game.folders.find((el) => el.name == "GMDeck");
+  if (!gmdeckid && game.user.isGM) {
+    console.log("PT | Create GM Deck");
+    let sampleDeckBlob = await (
+      await fetch("systems/peculiartales/decks/GMDeck.zip")
+    ).blob();
+    let sampleDeckFile = new File([sampleDeckBlob], "GMDeck.zip");
+    let deckImgBlob = await (
+      await fetch(`systems/peculiartales/decks/gm_back.png`)
+    ).blob();
+    let deckImgFile = new File([deckImgBlob], "deckimg.png");
+    let deck = game.decks.create(sampleDeckFile, deckImgFile);
+    deck.then(d => {
+      game.gmdeck = game.decks.get(d);
+      game.gmdeck.shuffle();
+    });
+  }
+  else {
+    game.gmdeck = game.decks.get(gmdeckid._id);
+    game.gmdeck.shuffle();
+  }
+
+  const playerdeckid = game.folders.find((el) => el.name == "PlayerDeck");
+  if (!playerdeckid && game.user.isGM) {
+    console.log("PT | Create Player Deck");
+    let sampleDeckBlob = await (
+      await fetch("systems/peculiartales/decks/PlayerDeck.zip")
+    ).blob();
+    let sampleDeckFile = new File([sampleDeckBlob], "PlayerDeck.zip");
+    let deckImgBlob = await (
+      await fetch(`systems/peculiartales/decks/player_back.png`)
+    ).blob();
+    let deckImgFile = new File([deckImgBlob], "deckimg.png");
+    let deck = game.decks.create(sampleDeckFile, deckImgFile);
+    deck.then(d => {
+      game.playerdeck = game.decks.get(d);
+      game.playerdeck.shuffle();
+    });
+  }
+  else{
+    game.playerdeck = game.decks.get(playerdeckid._id);
+    game.playerdeck.shuffle();
+  }
+});
+
+Hooks.once("ready", async function() {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on("hotbarDrop", (bar, data, slot) => createPeculiarTalesMacro(data, slot));
+
+
+});
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createPeculiarTalesMacro(data, slot) {
+  if (data.type !== "Item") return;
+  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+  const item = data.data;
+
+  // Create the macro command
+  const command = `game.peculiartales.rollItemMacro("${item.name}");`;
+  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: "script",
+      img: item.img,
+      command: command,
+      flags: { "peculiartales.itemMacro": true }
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {string} itemName
+ * @return {Promise}
+ */
+function rollItemMacro(itemName) {
+  const speaker = ChatMessage.getSpeaker();
+  let actor;
+  if (speaker.token) actor = game.actors.tokens[speaker.token];
+  if (!actor) actor = game.actors.get(speaker.actor);
+  const item = actor ? actor.items.find(i => i.name === itemName) : null;
+  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+  // Trigger the item roll
+  return item.roll();
+}
